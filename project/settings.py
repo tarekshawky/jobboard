0aed21e7a8bb9cb665fdfd284f093ae20b72aea5
tarek shawky <tarekshawky2016@gmail.com>
2021-07-10 05:54:38 +0200

    f

--- project/settings.py
+++ project/settings.py
@@ -1,7 +1,9 @@
-from pathlib import Path
-
 import os
 import django_heroku
+
+from pathlib import Path
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
